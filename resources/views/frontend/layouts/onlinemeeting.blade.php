b7d430692db412dad39a4a7fce02cb8e2f46ec18
Skyerp <skyerp.skylabtech@gmail.com>
2026-05-18 11:06:08 +0530

    content changes

--- resources/views/frontend/layouts/onlinemeeting.blade.php
+++ resources/views/frontend/layouts/onlinemeeting.blade.php
@@ -785,7 +785,7 @@
 
         $('#slotContainer').html('Loading...');
 
-        $.get('/overseas/get-slots/'+date,function(data){
+        $.get('/get-slots/'+date,function(data){
 
             let html='';
 
@@ -831,7 +831,7 @@
 
         $.ajax({
 
-            url:'/overseas/book',
+            url:'/book',
 
             type:'POST',
 
